7be53bed0779dfc1eefd5a5ce0d096d54bf29e4e
Lucas Tran <lucastran1107@gmail.com>
2025-04-25 17:34:07 +0200

    feat: :tada: set up a new next.js project

---
+++ frontend/my-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+    return (
+        
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <h1 className="text-4xl font-bold text-center sm:text-left">
+                    Welcome to the Quiz App!
+                </h1>
+                <p className="text-lg text-center sm:text-left">
+                    This is a simple example of a Next.js application.
+                </p>
+            </main>
+    );
+}
